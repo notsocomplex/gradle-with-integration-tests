46f951df44a5dc3a4f563facdc6785b35f69512e
Jean Croteau <jean.croteau@hybris.com>
2015-04-06 12:10:31 -0400

    Initial commit

---
+++ src/integrationTest/java/com/notsocomplex/MyIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.notsocomplex;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyIntegrationTest {
+
+	@Test
+	public void testSomething() {
+		System.out.println("I AM AN INTEGRATION TEST!");
+		Assert.assertEquals(true, true);
+	}
+	
+}
---
+++ src/test/java/com/notsocomplex/MyUnitTest.java
@@ -0,0 +1,14 @@
+package com.notsocomplex;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyUnitTest {
+	
+	@Test
+	public void testSomething() {		
+		System.out.println("I AM A UNIT TEST!");
+		Assert.assertEquals(true, true);
+	}
+	
+}
